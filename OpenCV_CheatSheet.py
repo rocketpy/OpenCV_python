a5e6f45dff466fa494b9e0d7b4e8e93e30ced3bf
rocketpy <dimitr.ushakov@gmail.com>
2021-01-31 21:08:40 +0200

    Update OpenCV_CheatSheet.py

--- OpenCV_CheatSheet.py
+++ OpenCV_CheatSheet.py
@@ -7,3 +7,16 @@ cv2.imshow("Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+#  IMPORTANT
+# On reading images this way via openCV, it isn’t in RGB colorspace—it’s in BGR !!!
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+#  best practice using a method
+def view_image(image, name_of_window):
+    cv2.namedWindow(name_of_window, cv2.WINDOW_NORMAL)
+    cv2.imshow(name_of_window, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+ 
