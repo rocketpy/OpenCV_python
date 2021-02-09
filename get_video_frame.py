ae28606a9c8e3b4ea2d680510d94804ef584c565
rocketpy <dimitr.ushakov@gmail.com>
2021-02-09 17:54:06 +0200

    Update get_video_frame.py

--- get_video_frame.py
+++ get_video_frame.py
@@ -6,8 +6,14 @@ cap = cv2.VideoCapture(some_video)
 
 # number of frames in the video
 # print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
 current_frame = 0
 
 ret, frame = cap.read()
 
+while ret:
+    cv2.imwrite('frames/frame' + str(current_frame) + '.jpg', frame)
+    current_frame += 1
+    ret, frame = cap.read()
+
+cap.release()
+ 
