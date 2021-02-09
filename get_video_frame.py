d66bf5ffbe7e21f4e8f44a43dc31e227cd199000
rocketpy <dimitr.ushakov@gmail.com>
2021-02-09 17:52:33 +0200

    Update get_video_frame.py

--- get_video_frame.py
+++ get_video_frame.py
@@ -1,1 +1,13 @@
+import cv2
+
+
+some_video = 'name_video.avi'
+cap = cv2.VideoCapture(some_video)
+
+# number of frames in the video
+# print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+current_frame = 0
+
+ret, frame = cap.read()
 
