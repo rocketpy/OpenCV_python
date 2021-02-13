c7a99c7667c3b241d9227911116af89de901827a
rocketpy <dimitr.ushakov@gmail.com>
2021-02-13 22:08:07 +0200

    Update OpenCV_CheatSheet.py

--- OpenCV_CheatSheet.py
+++ OpenCV_CheatSheet.py
@@ -137,6 +137,25 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 """
 
+#  Compare pictures
+# bitwise - compare pictures, show where is common
+import cv2
+import numpy as np
+
+
+img1 = np.zeros((250,500,3))
+img2 = np.zeros((250,500,3))
+
+img1 = cv2.rectangle(img1, (200,0), (300,100), (255,255,255), -1)
+img2 = cv2.rectangle(img2, (250, 0), (500,250), (255,255,255),-1)
+
+bitAnd = cv2.bitwise_and(img2, img1)
+bitOr = cv2.bitwise_or(img2, img1)
+bitXOR = cv2.bitwise_xor(img2, img1)
+
+bitNot1 = cv2.bitwise_not(img1)
+bitNot2 = cv2.bitwise_not(img2)
+
 
 #  Example , with path and directories
 import os
