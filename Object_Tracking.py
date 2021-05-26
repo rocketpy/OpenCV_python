3a14b9e2453480d3bf6613f53bc1177503fa8b15
rocketpy <dimitr.ushakov@gmail.com>
2021-05-26 23:06:57 +0300

    Update Object_Tracking.py

--- Object_Tracking.py
+++ Object_Tracking.py
@@ -1,1 +1,19 @@
+import cv2
 
+
+tracker = cv2.TrackerMOSSE_create()
+
+#  Tracker Types:
+
+# tracker = cv2.TrackerBoosting_create()
+# tracker = cv2.TrackerMIL_create()
+# tracker = cv2.TrackerKCF_create()
+# tracker = cv2.TrackerTLD_create()
+# tracker = cv2.TrackerMedianFlow_create()
+# tracker = cv2.TrackerCSRT_create()
+
+cap = cv2.VideoCapture(1)
+
+success, frame = cap.read()
+bbox = cv2.selectROI("Tracking", frame, False)
+tracker.init(frame, bbox)
