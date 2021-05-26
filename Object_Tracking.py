122035795471b985fa9c215cd6e7c868853692e6
rocketpy <dimitr.ushakov@gmail.com>
2021-05-26 23:13:18 +0300

    Update Object_Tracking.py

--- Object_Tracking.py
+++ Object_Tracking.py
@@ -1,10 +1,8 @@
 import cv2
 
 
-tracker = cv2.TrackerMOSSE_create()
-
 #  Tracker Types:
-
+# tracker = cv2.TrackerMOSSE_create()
 # tracker = cv2.TrackerBoosting_create()
 # tracker = cv2.TrackerMIL_create()
 # tracker = cv2.TrackerKCF_create()
@@ -12,8 +10,42 @@ tracker = cv2.TrackerMOSSE_create()
 # tracker = cv2.TrackerMedianFlow_create()
 # tracker = cv2.TrackerCSRT_create()
 
+tracker = cv2.TrackerMOSSE_create()
+
 cap = cv2.VideoCapture(1)
 
 success, frame = cap.read()
 bbox = cv2.selectROI("Tracking", frame, False)
 tracker.init(frame, bbox)
+
+
+def draw_box(img, bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 3 )
+    cv2.putText(img, "Tracking", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    
+    
+while True:
+    timer = cv2.getTickCount()
+    success, img = cap.read()
+    success, bbox = tracker.update(img)
+
+    if success:
+        draw_box(img, bbox)
+    else:
+        cv2.putText(img, "Lost", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    cv2.rectangle(img, (15, 15), (200, 90), (255, 0, 255), 2)
+    cv2.putText(img, "Fps:", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+    cv2.putText(img, "Status:", (20, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    if fps>60: myColor = (20, 230, 20)
+    elif fps>20: myColor = (230, 20, 20)
+    else: myColor = (20, 20, 230)
+    cv2.putText(img,str(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, myColor, 2)
+
+    cv2.imshow("Tracking", img)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
