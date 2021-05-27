65c47d976f3c55aed01185485344cf5c5398de76
rocketpy <dimitr.ushakov@gmail.com>
2021-05-27 21:26:44 +0300

    Update and rename CV2-tools.py to CV2_tools.py

---
+++ CV2_tools.py
@@ -0,0 +1,24 @@
+#  cv2-tools - Library to help the drawing process with OpenCV. Thought to add labels to the images. Classification of images, etc.
+
+# PyPi: https://pypi.org/project/cv2-tools/
+# Github: https://github.com/fernaper/cv2-tools
+
+# pip install cv2-tools
+
+#  Installation
+"""
+You will need to install:
+
+   1. opencv >= 3.6.2
+   2. numpy >= 1.13.3
+   3. python-constraint >= 1.4.0
+
+You can simply execute: pip install -r requirements.txt
+
+Finally you can install the library with:
+
+pip install cv2-tools
+
+When you install cv2-tools, it will automatically download numpy but not opencv becouse in some cases you will need another version.
+"""
+
