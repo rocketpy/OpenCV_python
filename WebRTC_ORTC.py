19a42c74769bae6fea04d4ee7a128f36aa2a9177
rocketpy <dimitr.ushakov@gmail.com>
2021-05-29 21:26:53 +0300

    Update WebRTC_ORTC.py

--- WebRTC_ORTC.py
+++ WebRTC_ORTC.py
@@ -1,1 +1,12 @@
+#  aiortc - An implementation of WebRTC and ORTC
+"""
+What is aiortc?
 
+aiortc is a library for Web Real-Time Communication (WebRTC) and Object Real-Time Communication (ORTC) in Python.
+It is built on top of asyncio, Python’s standard asynchronous I/O framework.
+"""
+
+# PyPi: https://pypi.org/project/aiortc/
+# Github: https://github.com/aiortc/aiortc
+
+# pip install aiortc
