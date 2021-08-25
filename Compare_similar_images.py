24456f846af01d35bbae0d986ed034d2789fa0be
rocketpy <dimitr.ushakov@gmail.com>
2021-08-25 22:16:03 +0300

    Update Compare_similar_images.py

--- Compare_similar_images.py
+++ Compare_similar_images.py
@@ -1,1 +1,16 @@
+# Using ImageHash
+import imagehash
+from PIL import Image
+
+
+hash_1 = imagehash.average_hash(Image.open('image_name.jpg')) 
+hash_2 = imagehash.average_hash(Image.open('image_name_2.jpg')) 
+
+cutoff = 5  # maximum bits that could be different between the hashes. 
+
+if hash_1 - hash_2 < cutoff:
+  print('Images are similar')
+else:
+  print('Images are not similar')
+ 
 
