64b1335430229770fe0235de5c099f8e0504404d
rocketpy <dimitr.ushakov@gmail.com>
2021-08-26 23:13:53 +0300

    Update Compare_similar_images.py

--- Compare_similar_images.py
+++ Compare_similar_images.py
@@ -15,6 +15,7 @@ else:
 
 
 #  without openCV
+# IMPORTANT - if you have colored pictures you have to do this in all 3 dimensions or just compare a greyscaled version !!!
 import numpy as np
 
 
@@ -23,3 +24,11 @@ pic_2 = np.random.rand(100, 100)
 pic_1_norm = pic_1/np.sqrt(np.sum(picture1**2))
 pic_2_norm = pic_2/np.sqrt(np.sum(picture2**2))
 
+# If compare similar pictures the sum will return 1
+print(np.sum(pic_1_norm**2))
+# result: 1.0
+
+# If they aren't similar, we'll get a value between 0 and 1 (a percentage if you multiply by 100)
+print(np.sum(pic_2_norm*pic_1_norm))
+# result: 0.82362519392365335
+ 
