cb4d5b89332174706a25c9c1755d7995fd69a4d5
rocketpy <dimitr.ushakov@gmail.com>
2021-08-27 23:38:27 +0300

    Update Compare_similar_images.py

--- Compare_similar_images.py
+++ Compare_similar_images.py
@@ -54,6 +54,29 @@ image = cv2.imread('food.jpeg')
 gray_image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 histogram2 = cv2.calcHist([gray_image2], [0],
                           None, [256], [0, 256])
+
+  
+c1, c2 = 0, 0
+  
+# Euclidean Distance between data1 and test
+i = 0
+while i<len(histogram) and i<len(histogram1):
+    c1+=(histogram[i]-histogram1[i])**2
+    i+= 1
+c1 = c1**(1 / 2)
+  
+ 
+# Euclidean Distance between data2 and test
+i = 0
+while i<len(histogram) and i<len(histogram2):
+    c2+=(histogram[i]-histogram2[i])**2
+    i+= 1
+c2 = c2**(1 / 2)
+  
+if(c1<c2):
+    print("data1.jpg is more similar to test.jpg as compare to data2.jpg")
+else:
+    print("data2.jpg is more similar to test.jpg as compare to data1.jpg")
   
   
 
